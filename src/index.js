30f29326649205e2358738a7055caf6990564590
Saodat <saodat.riskiyeva@gmail.com>
2025-05-05 08:52:45 -0400

    improve reusability with props

--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import StarRating from "./StarRating";
+import { useState } from "react";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={10} />
+    <StarRating
+      size={36}
+      color="green"
+      classNamr="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
