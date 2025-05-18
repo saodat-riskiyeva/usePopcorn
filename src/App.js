59d2ffb6c1b9236628de89fff687433534f2a720
Saodat <saodat.riskiyeva@gmail.com>
2025-05-17 22:13:46 -0400

    getting data from local storage on the first render

--- src/App.js
+++ src/App.js
@@ -56,10 +56,15 @@ const KEY = "8aa2426a";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  // const [watched, setWatched] = useState(tempWatchedData);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
