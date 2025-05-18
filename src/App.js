b840056211058ef3ae23134034b0a7f9114874e4
Saodat <saodat.riskiyeva@gmail.com>
2025-05-17 22:59:44 -0400

    adding a feature of clearing the search query at hit enter key

--- src/App.js
+++ src/App.js
@@ -472,9 +472,22 @@ function Search({ query, setQuery }) {
 
   const inputEl = useRef(null);
 
-  useEffect(function () {
-    inputEl.current.focus();
-  }, []);
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
 
   return (
     <input
